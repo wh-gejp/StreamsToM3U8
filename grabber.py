0dd5779387bd477806fc361741e83e1e1d40430a
David Park <dp247@rocketmail.com>
2024-03-28 10:19:05 +0000

    Add support for Dailymotion and Twitch streams

--- grabber.py
+++ grabber.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 import os
 from datetime import datetime, timedelta
+from urllib.parse import urlparse
 
 import pytz
 import requests
@@ -73,9 +74,9 @@ Build an XMLTV file based on provided stream information
     return etree.tostring(data, pretty_print=True, encoding='utf-8')
 
 
-def grab(url: str):
+def grab_youtube(url: str):
     """
-Grabs the live-streaming M3U8 file
+Grabs the live-streaming M3U8 file from YouTube
     :param url: The YouTube URL of the livestream
     """
     if '&' in url:
@@ -88,7 +89,7 @@ Grabs the live-streaming M3U8 file
 
 
     if '.m3u8' not in response or stream_info.status_code != 200:
-        print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
+        print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
         return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -108,19 +109,71 @@ Grabs the live-streaming M3U8 file
             tuner += 5
     print(f"{link[start: end]}")
 
+def grab_dailymotion(url: str):
+    """
+Grabs the live-streaming M3U8 file from Dailymotion at its best resolution
+    :param url: The Dailymotion URL of the livestream
+    :return:
+    """
+    requests.packages.urllib3.disable_warnings()
+    stream_info = requests.get(url, timeout=15)
+    response = stream_info.text
+    soup = BeautifulSoup(stream_info.text, features="html.parser")
+
+    if stream_info.status_code != 200:
+        print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
+        return
+
+    stream_title = soup.find("meta", property="og:title")["content"].split('-')[0].strip()
+    stream_desc = soup.find("meta", property="og:description")["content"]
+    stream_image_url = soup.find("meta", property="og:image")["content"]
+    channels.append((channel_name, channel_id, category, stream_title, stream_desc, stream_image_url))
+
+    stream_api = requests.get(f"https://www.dailymotion.com/player/metadata/video/{url.split('/')[4]}").json()['qualities']['auto'][0]['url']
+    m3u_file = requests.get(stream_api).text.strip().split('\n')[1:]
+    best_url = sorted([[int(m3u_file[i].strip().split(',')[2].split('=')[1]), m3u_file[i + 1]] for i in range(0, len(m3u_file) - 1, 2)], key=lambda x: x[0])[-1][1].split('#')[0]
+    print(best_url)
+
+def grab_twitch(url: str):
+    """
+
+    :param url:
+    :return:
+    """
+    requests.packages.urllib3.disable_warnings()
+    stream_info = requests.get(url, timeout=15)
+    soup = BeautifulSoup(stream_info.text, features="html.parser")
+
+    if stream_info.status_code != 200:
+        print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
+        return
+
+    stream_title = soup.find("meta", property="og:title")["content"].split('-')[0].strip()
+    stream_desc = soup.find("meta", property="og:description")["content"]
+    stream_image_url = soup.find("meta", property="og:image")["content"]
+    channels.append((channel_name, channel_id, category, stream_title, stream_desc, stream_image_url))
+
+    response = requests.get(f"https://pwn.sh/tools/streamapi.py?url={url}").json()["success"]
+    if response == "false":
+        print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
+        return
+    url_list = requests.get(f"https://pwn.sh/tools/streamapi.py?url={url}").json()["urls"]
+    max_res_key = list(url_list)[-1]
+    stream_url = url_list.get(max_res_key)
+    print(stream_url)
 
 channel_name = ''
 channel_id = ''
 category = ''
 
 # Open text file and parse stream information and URL
-with open('./youtubeLink.txt', encoding='utf-8') as f:
+with open('./streams.txt', encoding='utf-8') as f:
     print("#EXTM3U")
     for line in f:
         line = line.strip()
         if not line or line.startswith('##'):
             continue
-        if not line.startswith('https:'):
+        if not (line.startswith('https:') or line.startswith('http:')):
             line = line.split('||')
             channel_name = line[0].strip()
             channel_id = line[1].strip()
@@ -128,7 +181,12 @@ with open('./youtubeLink.txt', encoding='utf-8') as f:
             print(
                 f'\n#EXTINF:-1 tvg-id="{channel_id}" tvg-name="{channel_name}" group-title="{category}", {channel_name}')
         else:
-            grab(line)
+            if urlparse(line).netloc == 'www.youtube.com':
+                grab_youtube(line)
+            elif urlparse(line).netloc == 'www.dailymotion.com':
+                grab_dailymotion(line)
+            elif urlparse(line).netloc == 'www.twitch.tv':
+                grab_twitch(line)
 
 # Time to build an XMLTV file based on stream data
 channel_xml = build_xml_tv(channels)
