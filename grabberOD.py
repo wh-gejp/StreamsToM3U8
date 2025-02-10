c563e4bdc4db35f18f5da4525ab1188f73c7361d
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-10 19:15:20 +0100

    Update grabberOD.py

--- grabberOD.py
+++ grabberOD.py
@@ -21,7 +21,9 @@ Grabs the live-streaming M3U8 file from YouTube
     soup = BeautifulSoup(stream_info.text, features="html.parser")
 
     if '.m3u8' not in response or stream_info.status_code != 200:
+        print(f'\nRequest    : {url}')
         print(f'\nStatus Code: {stream_info.status_code}')
+        print("Response:")
         print(response)
         print("https://github.com/ExperiencersInternational/tvsetup/raw/main/staticch/no_stream_2.mp4")
         return
