a7559f92081c8c653d434e1e39072b6c21c6ac30
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-10 10:51:34 +0100

    Update exec_grabbOD.sh

--- exec_grabbOD.sh
+++ exec_grabbOD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m pip install requests
-python3 -m pip install html5lib
+python3 -m pip install bs4
 python3 -m pip install beautifulsoup4
 
 python3 grabberOD.py euronews.txt > ./euronews.m3u
