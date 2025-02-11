c0887f770fe4bd45c088af650b225b4e2265b475
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-11 12:34:11 +0100

    Update exec_grabber.sh

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -5,8 +5,4 @@ python3 -m pip install pytz
 python3 -m pip install beautifulsoup4
 
 python3 grabber.py euronews.txt > ./euronews.m3u
-cat ./iptv1.m3u > ./iptv_liste.m3u
-cat ./euronews.m3u >> ./iptv_liste.m3u
-cat ./iptv2.m3u >> ./iptv_liste.m3u
-
 echo M3U update complete.
