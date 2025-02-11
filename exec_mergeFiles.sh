d6a3b5d5f63725a2eef7a5eca111858dbdcbd109
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-11 18:15:50 +0100

    Update exec_mergeFiles.sh

--- exec_mergeFiles.sh
+++ exec_mergeFiles.sh
@@ -3,5 +3,5 @@ pwd
 cat ./iptv1.m3u > ./iptv_liste.m3u
 cat ./euronews.m3u >> ./iptv_liste.m3u
 cat ./iptv2.m3u >> ./iptv_liste.m3u
-
+cat ./dw.m3u >> ./iptv_liste.m3u
 echo M3U files merge complete.
