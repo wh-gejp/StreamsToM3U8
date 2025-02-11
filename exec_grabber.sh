3f3c5a6cf4682fdf5b37050d68f06dacaee528c3
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-11 17:34:10 +0100

    Update exec_grabber.sh

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -3,6 +3,5 @@ python3 -m pip install requests
 python3 -m pip install lxml
 python3 -m pip install pytz
 python3 -m pip install beautifulsoup4
-pwd
 python3 grabber.py euronews.txt > ./euronews.m3u
 echo M3U update complete.
