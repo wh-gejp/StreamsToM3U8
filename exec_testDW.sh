7006bffbac88091c0ded36d65bdcd4df245728d3
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-15 16:13:13 +0100

    Update exec_testDW.sh

--- exec_testDW.sh
+++ exec_testDW.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m pip install requests
 python3 -m pip install fake-useragent
-python3 testDW_URL.py 
-echo M3U update complete.
+python3 testDW_URL.py > testDW.txt
+echo testDW.txt complete.
