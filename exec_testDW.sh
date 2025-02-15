82f8ad457c22935b512e6fc9fa08ee8ac96fca60
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-15 16:43:44 +0100

    Update exec_testDW.sh

--- exec_testDW.sh
+++ exec_testDW.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 -m pip install requests
 python3 -m pip install fake-useragent
+python3 -m pip install beautifulsoup4
 python3 testDW_URL.py > testDW.txt
 echo testDW.txt complete.
