5de73c6dbf4a6b4fe4e580866add835ec27da25d
wh-gejp <jp.wallersheim@gmail.com>
2025-02-17 16:58:05 +0100

    Update exec_testDW.sh

--- exec_testDW.sh
+++ exec_testDW.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+$User = $1
 python3 -m pip install requests
 python3 -m pip install fake-useragent
 python3 -m pip install beautifulsoup4
-python3 testDW_URL.py > testDW.txt
+python3 testDW_URL.py  $User> testDW.txt
 echo testDW.txt complete.
