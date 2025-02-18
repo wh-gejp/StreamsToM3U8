c5a4cdbf2b6cfa292b744769043416ae1f5dfebe
wh-gejp <jp.wallersheim@gmail.com>
2025-02-18 07:58:16 +0100

    Update exec_testDW.sh

--- exec_testDW.sh
+++ exec_testDW.sh
@@ -2,7 +2,7 @@
 User=$1
 Secret=$2
 echo "User: ${User} :"
-echo "Secret User: ${{ secrets.MAIL_USER}} :"
+# echo "Secret User: ${{ secrets.MAIL_USER}} :"
 python3 -m pip install requests
 python3 -m pip install fake-useragent
 python3 -m pip install beautifulsoup4
