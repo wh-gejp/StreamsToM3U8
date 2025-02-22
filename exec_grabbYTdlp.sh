3f1ec2c3bfba5ac9373168c8fe85aa99b80156b4
wh-gejp <jp.wallersheim@gmail.com>
2025-02-22 17:14:35 +0100

    Update exec_grabbYTdlp.sh

--- exec_grabbYTdlp.sh
+++ exec_grabbYTdlp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-./yt-dlp_2025.02.19 -f 95 -g http://www.youtube.com/watch?v=CQ3KsEbsYHs > testYTdlp.txt
+User=$1
+Secret=$2
+echo "User: ${User} :"
+# echo "Secret User: ${{ secrets.MAIL_USER}} :" # git hub Variable will not be substituted
+./yt-dlp_2025.02.19 -u ${User} -p ${Secret} -f 95 -g http://www.youtube.com/watch?v=CQ3KsEbsYHs > testYTdlp.txt
 echo testYTdlp.txt complete.
